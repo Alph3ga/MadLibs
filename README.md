9eed86ab284d4500a0581b520c8056758641c4dc
Alph3ga <akashneelrc@gmail.com>
2022-10-08 13:03:50 +0530

    commented some code

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## This is a Python implementation of the game MadLibs
 
 - To run, just git clone this repository, and run main.py
-- The NTLK library has been used for sentence tokenization and part of speech tagging
+- The NLTK library has been used for sentence tokenization and part of speech tagging
 - PyQT6 is used, along with QT Designer and pycui6 to create the GUI
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QFormLayout, QLabel, QLineEdit, QWidget, QPushButton
+import os
+import random
+import sys
+
 from PyQt6 import QtCore, QtGui
-from UIgen import Ui_MainWindow
-from madlibui import Ui_MainWindow as UI_MainWindow
-from nltk.tag import pos_tag
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFormLayout, QLabel, QLineEdit, QWidget, QPushButton
 from nltk import download
+from nltk.tag import pos_tag
 from nltk.tokenize import word_tokenize
 from nltk.tokenize.treebank import TreebankWordDetokenizer
-import random
 
+from UIgen import Ui_MainWindow
+from madlibui import Ui_MainWindow as UI_MainWindow
+
+# downloading/updating the NLTK databases
 download('punkt')
 download('averaged_perceptron_tagger')
 
-def resource_path(relative_path): #use this for resource path for auto-py-to-exe
+
+def resource_path(relative_path):  # use this for resource path for auto-py-to-exe
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
@@ -21,15 +27,16 @@ def resource_path(relative_path): #use this for resource path for auto-py-to-exe
 
     return os.path.join(base_path, relative_path)
 
-def get_tokenized():
+
+def get_tokenized():  #gets a random story from the file and tokenizes it
     global token_story
-    f=open("stories.biz","r",encoding="utf-8")
-    num=random.randint(1,50)
-    str=""
+    f = open("stories.biz", "r", encoding="utf-8")
+    num = random.randint(1, 50)
+    str = ""
     for i in range(num):
-        str=f.readline()
-    token_story=word_tokenize(str)
-    lst= pos_tag(token_story)
+        str = f.readline()
+    token_story = word_tokenize(str)
+    lst = pos_tag(token_story)
     return lst
 
 
@@ -39,11 +46,13 @@ class StartWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.pushButton.clicked.connect(play_now)
 
+
 class MadLibWindow(QMainWindow, UI_MainWindow):
     def __init__(self):
         super(MadLibWindow, self).__init__()
         self.setupUi(self)
         self.pushButton.clicked.connect(restart)
+
 
 class ChoiceWindow(QMainWindow):
     def __init__(self):
@@ -51,10 +60,10 @@ class ChoiceWindow(QMainWindow):
         self.setWindowTitle("MadLibs")
         self.resize(800, 600)
         self.setMinimumSize(QtCore.QSize(100, 100))
-        form= QFormLayout()
+        form = QFormLayout()
 
-        self.labels=[]
-        self.textFields=[]
+        self.labels = []
+        self.textFields = []
 
         font = QtGui.QFont()
         font.setFamily("Georgia")
@@ -64,15 +73,15 @@ class ChoiceWindow(QMainWindow):
             self.labels.append(QLabel())
             self.textFields.append(QLineEdit())
             self.labels[i].setFont(font)
-            form.addRow(self.labels[i],self.textFields[i])
+            form.addRow(self.labels[i], self.textFields[i])
 
         font.setPointSize(10)
-        self.getStoryButton=QPushButton("Get MadLib")
+        self.getStoryButton = QPushButton("Get MadLib")
         self.getStoryButton.clicked.connect(self.getMadLib)
         self.getStoryButton.setFont(font)
         form.addRow(self.getStoryButton)
 
-        wid= QWidget()
+        wid = QWidget()
         wid.setLayout(form)
         self.setCentralWidget(wid)
 
@@ -80,59 +89,64 @@ class ChoiceWindow(QMainWindow):
         global str_tokens, detokenizer, story
 
         for i in range(15):
-            token_story[str_choices[i][1]]=self.textFields[i].text()
-        story= detokenizer.detokenize(token_story)
+            token_story[str_choices[i][1]] = self.textFields[i].text()
+        story = detokenizer.detokenize(token_story)
         showMadLib()
 
-def showMadLib():
+
+def showMadLib():  # this is the function to show the final madlib window
     global window
-    window= MadLibWindow()
+    window = MadLibWindow()
     window.label.setText(story)
     window.show()
 
 
-def play_now():
+def play_now():  # this is the function to show the window to enter the madlib choices
     global window, str_tokens, str_choices, tag_name
-    window= ChoiceWindow()
+    window = ChoiceWindow()
 
-    str_tokens= get_tokenized()
-    str_choices= get_choices(str_tokens)
+    str_tokens = get_tokenized()
+    str_choices = get_choices(str_tokens)
 
     for i in range(15):
         window.labels[i].setText(tag_name[str_choices[i][0]])
     window.show()
 
-def get_choices(str_tokens):
+
+def get_choices(str_tokens):  # get the valid choices of words to be replaced as (POS_TAG, index)
+
     global valid_tags
-    lst=[]
+    lst = []
     for i in range(len(str_tokens)):
         if str_tokens[i][1] in valid_tags:
-            lst.append([str_tokens[i][1],i])
-    lst=random.sample(lst,15)
+            lst.append([str_tokens[i][1], i])
+    lst = random.sample(lst, 15)
     return lst
+
 
 def restart():
     global window
-    window= StartWindow()
+    window = StartWindow()
     window.show()
 
+
 if __name__ == "__main__":
-    str_tokens=[]
-    str_choices=[]
-    token_story=[]
-    story=""
+    str_tokens = []
+    str_choices = []
+    token_story = []
+    story = ""
     detokenizer = TreebankWordDetokenizer()
-    valid_tags=['JJ','JJR','JJS','NN','NNS','NNP','NNPS','RB','VB','VBD','VBG']
-    tag_name={'JJ':'Adjective',
-              'JJR':'Comparative Adjevtive',
-              'JJS':'Superlative Adjective',
-              'NN':'Common Noun',
-              'NNS':'Common Noun Plural',
-              'NNP':'Proper Noun',
-              'NNPS':'Proper Noun Plural',
-              'RB':'Adverb','VB':'Verb Present Tense',
-              'VBD':'Verb Past Tense',
-              'VBG':'Verb Present Participle'}
+    valid_tags = ['JJ', 'JJR', 'JJS', 'NN', 'NNS', 'NNP', 'NNPS', 'RB', 'VB', 'VBD', 'VBG']
+    tag_name = {'JJ': 'Adjective',
+                'JJR': 'Comparative Adjevtive',
+                'JJS': 'Superlative Adjective',
+                'NN': 'Common Noun',
+                'NNS': 'Common Noun Plural',
+                'NNP': 'Proper Noun',
+                'NNPS': 'Proper Noun Plural',
+                'RB': 'Adverb', 'VB': 'Verb Present Tense',
+                'VBD': 'Verb Past Tense',
+                'VBG': 'Verb Present Participle'}
     app = QApplication([])
 
     window = StartWindow()
